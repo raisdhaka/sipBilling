dbb00d789710f6a7fb938b877db179a65a6b4c36
Rais Uddin <raisdhaka@gmail.com>
2025-02-11 13:17:00 +0600

    initial

---
+++ resources/views/layouts/leftmenu.blade.php
@@ -0,0 +1,88 @@
+<div class="vertical-menu">
+
+    <div data-simplebar class="h-100">
+
+        <!-- User details -->
+
+
+        <!--- Sidemenu -->
+        <div id="sidebar-menu">
+            <!-- Left Menu Start -->
+            <ul class="metismenu list-unstyled" id="side-menu">
+                {{-- <li class="menu-title">Menu</li> --}}
+
+                <li>
+                    <a href="index.html" class="waves-effect">
+                        <img src="{{asset('images/dash.svg')}}" width="24" alt="Dashboard">
+                        {{-- <span class="badge rounded-pill bg-success float-end">3</span> --}}
+                        <span>Dashboard</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="calendar.html" class=" waves-effect">
+                        <img src="{{asset('images/cust.svg')}}" width="24" alt="Customer">
+                        <span>Customer</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="calendar.html" class=" waves-effect">
+                        <img src="{{asset('images/plan.svg')}}" width="24" alt="Plan">
+                        <span>Rate plan</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="calendar.html" class=" waves-effect">
+                        <img src="{{asset('images/pref.svg')}}" width="24" alt="Prefix">
+                        <span>Add Prefix</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="calendar.html" class=" waves-effect">
+
+                        <span class="him">IP Truck</span>
+                        <span class="badge rounded-pill bg-success float-end">BETA</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="calendar.html" class=" waves-effect">
+                        <img src="{{asset('images/sipTrunk.svg')}}" width="24" alt="Sip Truck">
+                        <span>SIP Trunk</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="calendar.html" class=" waves-effect">
+                        <img src="{{asset('images/acCall.svg')}}" width="24" alt="Active call">
+                        <span>Active Call</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="calendar.html" class=" waves-effect">
+                        <img src="{{asset('images/frwl.svg')}}" width="24" alt="Firewall">
+                        <span>IP Firewall</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <img src="{{asset('images/management.svg')}}" width="24" alt="Management">
+                        <span>Management</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="">Inbox</a></li>
+                        <li><a href="">Read Email</a></li>
+                    </ul>
+                </li>
+
+
+
+
+
+
+
+
+
+            </ul>
+        </div>
+        <!-- Sidebar -->
+    </div>
+</div>
